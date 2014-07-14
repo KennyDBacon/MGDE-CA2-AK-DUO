8af2c27273f1cdd3413201eafcebc56d4b5d0d06
KennyDBacon <noobyken85@hotmail.com>
2014-07-14 11:47:54 +0800

    Final Codes

---
+++ RacingRage/gamestatemanagementsample/Screens/CreditsScreen.cs
@@ -0,0 +1,150 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input.Touch;
+using GameStateManagement;
+
+
+namespace GameStateManagementSample
+{
+    /// <summary>
+    /// Credits Screen
+    /// </summary>
+    class CreditsScreen : GameScreen
+    {
+
+
+        ContentManager content;
+        SpriteFont gameFont;
+        float creditpos;
+        String creditsTitle = "Credits";
+
+        Texture2D splogo;
+        Texture2D background_pattern;
+
+        InputAction backToMenu;
+
+        //Contains height and width of screen.
+        Vector2 ScreenDimensions = new Vector2(
+            GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height,
+            GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width);
+
+        //Vector2 creditsPosition = new Vector2()
+
+        public CreditsScreen()
+        {
+            EnabledGestures = GestureType.Tap | GestureType.DoubleTap;
+        }
+
+
+
+        public override void Activate(bool instancePreserved)
+        {
+
+            if (!instancePreserved)
+            {
+                if (content == null)
+                {
+                    content = new ContentManager(ScreenManager.Game.Services, "Content");
+                }
+                backToMenu = new InputAction(new Buttons[] { Buttons.Back }, null, true);
+                gameFont = content.Load<SpriteFont>("gamefont");
+                background_pattern = content.Load<Texture2D>("background");
+                splogo = content.Load<Texture2D>("SPLOGO");
+                
+
+                creditpos = ScreenDimensions.Y / 2 - gameFont.MeasureString(creditsTitle).X / 2;
+                TouchPanel.EnabledGestures =
+                GestureType.Tap |
+                GestureType.DoubleTap;
+
+
+                // once the load has finished, we use ResetElapsedTime to tell the game's
+                // timing mechanism that we have just finished a very long frame, and that
+                // it should not try to catch up.
+                ScreenManager.Game.ResetElapsedTime();
+            }
+        }
+
+
+        public override void Deactivate()
+        {
+            base.Deactivate();
+        }
+
+        public override void Update(GameTime gameTime, bool otherScreenHasFocus,
+                                                       bool coveredByOtherScreen)
+        {
+            base.Update(gameTime, otherScreenHasFocus, false);
+            creditpos--;
+
+            if (creditpos <= -100)
+            {
+                LoadingScreen.Load(ScreenManager, false, null, new BackgroundScreen(),
+                                                        new PhoneMainMenuScreen());
+            }
+
+
+        }
+        /// <summary>
+        /// Lets the game respond to player input. Unlike the Update method,
+        /// this will only be called when the gameplay screen is active.
+        /// </summary>
+        public override void HandleInput(GameTime gameTime, InputState input)
+        {
+
+            PlayerIndex player;
+
+            foreach (GestureSample gesture in input.Gestures)
+            {
+                // If we have a tap
+                if (gesture.GestureType == GestureType.Tap)
+                {
+
+                }
+                else if (gesture.GestureType == GestureType.DoubleTap)
+                {
+                    LoadingScreen.Load(ScreenManager, false, null, new BackgroundScreen(),
+                                                        new PhoneMainMenuScreen());
+                }
+            }
+
+            if (backToMenu.Evaluate(input, ControllingPlayer, out player))
+            {
+                LoadingScreen.Load(ScreenManager, false, null, new BackgroundScreen(),
+                                                        new PhoneMainMenuScreen());
+            }
+
+        }
+
+
+        /// <summary>
+        /// Allows the game component to perform any initialization it needs to before starting
+        /// to run.  This is where it can query for any required services and load content.
+        /// </summary>
+        public override void Draw(GameTime gameTime)
+        {
+
+            ScreenManager.SpriteBatch.Begin();
+            string credits = "AK-DUO Studios\n\nProgrammed By \nKenny (Head)\nAdam\n\nArt by\nAdam(Head)\n\nSchool of"+
+                "\nDigital Media\n&\nInfocomm Technology\n\nSound Effects from \nSoundBible.com\n\nFreesound.org\nqubodup";
+
+            ScreenManager.SpriteBatch.Draw(background_pattern,
+                new Vector2(0, 0),
+                Color.White);
+            
+            ScreenManager.SpriteBatch.DrawString(ScreenManager.Font, credits, new Vector2(50, creditpos), Color.White);
+            ScreenManager.SpriteBatch.Draw(splogo, new Vector2(50, (ScreenDimensions.Y / 2 + gameFont.MeasureString(credits).X)), Color.White);
+
+            ScreenManager.SpriteBatch.End();
+
+        }
+    }
+}
---
+++ RacingRage/gamestatemanagementsample/Screens/PhoneMainMenuScreen.cs
@@ -0,0 +1,134 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// PhoneMainMenuScreen.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+using System;
+using GameStateManagement;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Media;
+
+namespace GameStateManagementSample
+{
+    class PhoneMainMenuScreen : PhoneMenuScreen
+    {
+        public PhoneMainMenuScreen()
+            : base("Main Menu")
+        {
+            // Create a button to start the game
+            Button playButton = new Button("Play");
+            playButton.Tapped += playButton_Tapped;
+            MenuButtons.Add(playButton);
+
+            BooleanButton musicButton = new BooleanButton("Music", true);
+            musicButton.Tapped += musicButton_Tapped;
+            MenuButtons.Add(musicButton);
+
+            BooleanButton sfxButton = new BooleanButton("Sound", true);
+            sfxButton.Tapped += sfxButton_Tapped;
+            MenuButtons.Add(sfxButton);
+
+            BooleanButton accelerometerButton = new BooleanButton("Accelerometer", true);
+            accelerometerButton.Tapped += accelerometerButton_Tapped;
+            MenuButtons.Add(accelerometerButton);
+
+            Button creditsButton = new Button("Credits");
+            creditsButton.Tapped += creditsButton_Tapped;
+            MenuButtons.Add(creditsButton);
+        }
+
+        public override void Activate(bool instancePreserved)
+        {
+            checkButton();
+            if (ScreenManager.enableMusic == true)
+            {
+                MediaPlayer.Play(ScreenManager.cheesymusic);
+            }
+            base.Activate(instancePreserved);
+        }
+
+        void checkButton()
+        {
+            for (int i = 0; i < MenuButtons.Count; i++)
+            {
+                Button b = MenuButtons[i];
+
+                if(b.Text.Contains("Music"))
+                {
+                    if (ScreenManager.enableMusic == true)
+                    {
+                        b.Text = "Music: On";
+                    }
+                    else
+                    {
+                        b.Text = "Music: Off";
+                    }
+                }
+                else if (b.Text.Contains("Sound"))
+                {
+                    if (ScreenManager.enableSoundEffect == true)
+                        b.Text = "Sound: On";
+                    else
+                        b.Text = "Sound: Off";
+                }
+                else if (b.Text.Contains("Accelerometer"))
+                {
+                    if (ScreenManager.enableAccelerometer == true)
+                        b.Text = "Accelerometer: On";
+                    else
+                        b.Text = "Accelerometer: Off";
+                }
+            }
+        }
+
+        void playButton_Tapped(object sender, EventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, true, PlayerIndex.One, new GameplayScreen());
+        }
+
+        void creditsButton_Tapped(object sender, EventArgs e)
+        {
+            LoadingScreen.Load(ScreenManager, true, PlayerIndex.One, new CreditsScreen());
+        }
+
+        void musicButton_Tapped(object sender, EventArgs e)
+        {
+            BooleanButton button = sender as BooleanButton;
+            ScreenManager.enableMusic = !ScreenManager.enableMusic;
+
+            if (ScreenManager.enableMusic == false)
+            {
+                MediaPlayer.Pause();
+            }
+            else 
+            {
+                MediaPlayer.Resume();
+            }
+            checkButton();
+        }
+
+        void sfxButton_Tapped(object sender, EventArgs e)
+        {
+            BooleanButton button = sender as BooleanButton;
+            ScreenManager.enableSoundEffect = !ScreenManager.enableSoundEffect;
+            checkButton();
+        }
+
+        void accelerometerButton_Tapped(object sender, EventArgs e)
+        {
+            BooleanButton button = sender as BooleanButton;
+            ScreenManager.enableAccelerometer = !ScreenManager.enableAccelerometer;
+            checkButton();
+        }
+
+        protected override void OnCancel()
+        {
+            ScreenManager.Game.Exit();
+            base.OnCancel();
+        }
+    }
+}
